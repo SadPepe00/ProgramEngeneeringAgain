8466440d8467fb7d30d4942e20ee39a04c0205c5
SukhanDmitry <suhan.gg01@gmail.com>
2023-12-14 21:23:11 +0300

    wow its not working
    https://youtu.be/jTPy9brBdAM?si=iU6P53MRNpMWLNCK

--- Homework/Homework/Program.cs
+++ Homework/Homework/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("wow Ferm's theorem is true");
+            Console.WriteLine("wow Ferm's theorem is true")
+
         }
     }
 }
