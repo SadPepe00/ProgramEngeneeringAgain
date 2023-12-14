f5adcaf16f6a1c129b2a086022566d2f5785675e
SukhanDmitry <suhan.gg01@gmail.com>
2023-12-14 22:13:01 +0300

    Test update ver3

--- Homework/Tests/UnitTest1.cs
+++ Homework/Tests/UnitTest1.cs
@@ -9,5 +9,15 @@ namespace Tests
         {
             Assert.AreEqual(Program.TokyoGhoulIsGoodManga(),993);
         }
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(1,1);
+        }
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual(2,2);
+        }
     }
 }
