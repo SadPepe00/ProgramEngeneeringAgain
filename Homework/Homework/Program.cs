66ccbf4cb7509cb4bf1ca84ee65d3f854e1987c3
SukhanDmitry <suhan.gg01@gmail.com>
2023-12-14 21:41:37 +0300

    Tests added

--- Homework/Homework/Program.cs
+++ Homework/Homework/Program.cs
@@ -1,11 +1,13 @@
-﻿namespace Homework
+﻿using System.Diagnostics;
+namespace Homework
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("wow Ferm's theorem is true")
-
+            Console.WriteLine("wow Ferm's theorem is true");
+            Debug.Assert(1 == 1);
+            Debug.Assert('a'=='a');
         }
     }
 }
